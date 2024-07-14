9b4a60617b4a16c9f10a9c73caf5b1e76948df1d
Tulir Asokan <tulir@maunium.net>
2024-07-14 19:31:43 +0300

    variationselector: update to Unicode 15.1

--- variationselector/generate.sh
+++ variationselector/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -e "$(
-	curl -s https://www.unicode.org/Public/15.0.0/ucd/emoji/emoji-variation-sequences.txt \
+	curl -s https://www.unicode.org/Public/15.1.0/ucd/emoji/emoji-variation-sequences.txt \
 	| grep FE0F \
 	| awk '{ printf("\\U%8s\n", $1) }' \
 	| sed 's/ /0/g'
@@ -8,7 +8,7 @@ echo -e "$(
 
 # Why does this need a \n at the beginning to avoid eating the first emoji?!?!
 echo -e "\n$(
-	curl -s https://unicode.org/Public/emoji/15.0/emoji-test.txt \
+	curl -s https://unicode.org/Public/emoji/15.1/emoji-test.txt \
 	| grep '; fully-qualified' \
 	| grep FE0F \
 	| sed -E 's/\s+;.*//g' \
