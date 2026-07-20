73fd685ead1928ed765745bf3857a954e03193a2
timedout <git@nexy7574.co.uk>
2026-07-20 14:27:42 +0100

    dbutil: Add tag-based reflection scanner

--- dbutil/reflectscan.go
+++ dbutil/reflectscan.go
@@ -7,7 +7,10 @@
 package dbutil
 
 import (
+	"errors"
+	"fmt"
 	"reflect"
+	"slices"
 )
 
 func reflectScan[T any](row Scannable) (*T, error) {
@@ -27,4 +30,47 @@ func reflectScan[T any](row Scannable) (*T, error) {
 // This is a simplified implementation that always scans to all struct fields. It does not support any kind of struct tags.
 func NewSimpleReflectRowIter[T any](rows Rows, err error) RowIter[*T] {
 	return ConvertRowFn[*T](reflectScan[T]).NewRowIter(rows, err)
+}
+
+var ErrUnknownField = errors.New("column has no associated struct field")
+
+func reflectFieldScan[T any](columns []string) func(Scannable) (*T, error) {
+	return func(row Scannable) (*T, error) {
+		t := new(T)
+		val := reflect.ValueOf(t).Elem()
+		fields := reflect.VisibleFields(val.Type())
+		fieldMap := make(map[string][]int, len(fields))
+		for _, field := range fields {
+			colname := field.Tag.Get("column")
+			if colname == "" {
+				continue
+			}
+			if !slices.Contains(columns, colname) {
+				continue
+			}
+			fieldMap[colname] = field.Index
+		}
+
+		scanInto := make([]any, len(columns))
+		for i, column := range columns {
+			index, ok := fieldMap[column]
+			if !ok {
+				return nil, fmt.Errorf("%w: %s", ErrUnknownField, column)
+			}
+			scanInto[i] = val.FieldByIndex(index).Addr().Interface()
+		}
+		err := row.Scan(scanInto...)
+		return t, err
+	}
+}
+
+// NewComplicatedReflectRowIter creates a new RowIter that uses reflection to scan rows into the given type.
+//
+// It scans into the struct by determining which columns were returned in the query, searching struct fields for the
+// `colname` tag. Columns that have no associated struct field will return ErrUnknownField.
+// Struct fields that have an empty colname tag, or a tag value which is not in the column list, are skipped.
+func NewComplicatedReflectRowIter[T any](rows Rows, err error) RowIter[*T] {
+	var cols []string
+	cols, err = rows.Columns()
+	return ConvertRowFn[*T](reflectFieldScan[T](cols)).NewRowIter(rows, err)
 }
